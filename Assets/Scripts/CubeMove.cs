12c920ffb0e2ac4284b8b4688c27aa4b754fbbfc
raf_farinha <70711930+raf-lf@users.noreply.github.com>
2022-02-20 18:08:11 -0300

    Ogre :)

--- Assets/Scripts/CubeMove.cs
+++ Assets/Scripts/CubeMove.cs
@@ -7,18 +7,24 @@ public class CubeMove : MonoBehaviour
 {
     public CharacterController controller;
     public Transform cam;
+    private Animator anim;
 
     public float lerp;
     public float lerpVelocity;
     public float speed = 6f;
 
+
+    private void Start()
+    {
+        anim = GetComponentInChildren<Animator>();
+    }
     private void Update()
     {
         float h = Input.GetAxisRaw("Horizontal");
         float v = Input.GetAxisRaw("Vertical");
         Vector3 direction = new Vector3(h, 0f, v).normalized;
 
-        if(direction.magnitude >= 0.1f)
+        if (direction.magnitude >= 0.1f)
         {
             float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref lerpVelocity, lerp);
@@ -29,7 +35,12 @@ public class CubeMove : MonoBehaviour
             Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
 
             controller.Move(moveDir.normalized * speed * Time.deltaTime);
+
+            anim.SetBool("move", true);
         }
+        else
+            anim.SetBool("move", false);
+
     }
 
 }
