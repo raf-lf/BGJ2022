3d012effcd5aaa7726981b50d378ae5f3d27cc33
OBolsa <vitor-hug@hotmail.com>
2022-02-21 01:41:43 -0300

    Correção de nome de Script igual

--- Assets/Scripts/PlayerMovementTest.cs
+++ Assets/Scripts/PlayerMovementTest.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using Cinemachine;
 
-public class PlayerMovement : MonoBehaviour
+public class PlayerMovementTest : MonoBehaviour
 {
     private CharacterController controller;
     private Transform cam;
