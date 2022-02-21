81dd540f169e435d94262fe2169b2b66510fa7eb
OBolsa <vitor-hug@hotmail.com>
2022-02-21 16:51:15 -0300

    Corrigindo script

--- Assets/MousePointer/MousePointer.cs
+++ Assets/MousePointer/MousePointer.cs
@@ -6,31 +6,16 @@ using Cinemachine;
 
 public class MousePointer : MonoBehaviour
 {
-    public GameObject aim;
-
     private MouseTarget target;
     public LayerMask interactionLayer;
-    Vector3 hitPoint;
-
-    private void Start()
-    {
-        hitPoint = new Vector3(0f, 0f, 0f);
-    }
 
     private void Update()
     {
         CheckMouseRaycast();
-
-        aim.transform.position = hitPoint;
     }
 
     public void CheckMouseRaycast()
     {
-        if(Physics.Raycast(MouseWorldPosition(), out RaycastHit hitI))
-        {
-            hitPoint = hitI.point;
-        }
-
         if (Physics.Raycast(MouseWorldPosition(), out RaycastHit hit, interactionLayer) && hit.collider.CompareTag("Interagivel"))
         {
             target = hit.collider.gameObject.GetComponent<MouseTarget>();
