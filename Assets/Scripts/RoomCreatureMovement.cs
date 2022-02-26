1b90521e18096a6c64d1a29fc2800ff5c041ff61
OBolsa <vitor-hug@hotmail.com>
2022-02-26 14:01:23 -0300

    Sistema de Game Over

--- Assets/Scripts/RoomCreatureMovement.cs
+++ Assets/Scripts/RoomCreatureMovement.cs
@@ -8,8 +8,26 @@ public class RoomCreatureMovement : MonoBehaviour
     public List<RoomCreatureMovement> adjacentRooms;
     public List<Transform> spot;
 
+    private void Start()
+    {
+        UpdateSpots();
+    }
+
     public Transform GetRandomSpot()
     {
         return spot[Random.Range(0, spot.Count)];
+    }
+
+    [ContextMenu("Update Spots")]
+    public void UpdateSpots()
+    {
+        spot.Clear();
+
+        Transform[] childrensTransform = GetComponentsInChildren<Transform>();
+
+        foreach(Transform child in childrensTransform)
+        {
+            spot.Add(child);
+        }
     }
 }
