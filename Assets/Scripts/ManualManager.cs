a1281a7ab6c8835712d502ff5f4c89f4abdc3bdb
raf_farinha <70711930+raf-lf@users.noreply.github.com>
2022-02-26 22:42:16 -0300

    overlay

--- Assets/Scripts/ManualManager.cs
+++ Assets/Scripts/ManualManager.cs
@@ -7,15 +7,24 @@ public class ManualManager : MonoBehaviour
     public Animator manualAnim;
     public int pageQty;
     private bool paused;
+    public PlaySfx sfxConfirm1;
+    public PlaySfx sfxConfirm2;
+    public PlaySfx sfxCancel;
     
     public void GoPage(bool advance)
     {
         int currentPage = manualAnim.GetInteger("page");
 
         if (advance)
+        {
+            sfxConfirm1.PlayInspectorSfx();
             currentPage++;
+        }
         else
+        {
+            sfxConfirm2.PlayInspectorSfx();
             currentPage--;
+        }
 
         currentPage = Mathf.Clamp(currentPage, 1, pageQty);
 
@@ -28,9 +37,15 @@ public class ManualManager : MonoBehaviour
         paused = !paused;
 
         if (paused)
+        {
+            sfxConfirm2.PlayInspectorSfx();
             Time.timeScale = 0;
+        }
         else
+        {
+            sfxCancel.PlayInspectorSfx();
             Time.timeScale = 1;
+        }
 
     }
 }
