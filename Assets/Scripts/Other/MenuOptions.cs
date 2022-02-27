15c8846ffa9c763bcb464efb6c53210a41d93049
raf_farinha <70711930+raf-lf@users.noreply.github.com>
2022-02-27 03:50:17 -0300

    Coisinhas na tela de inicio

---
+++ Assets/Scripts/Other/MenuOptions.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuOptions : MonoBehaviour
+{
+    public Slider sfxSlider;
+    public Slider bgmSlider;
+
+    private void Start()
+    {
+        sfxSlider.value = AudioManager.volumeCurrentSfx;
+        bgmSlider.value = AudioManager.volumeCurrentBgm;
+        
+    }
+
+    public void UpdateVolumes()
+    {
+        AudioManager.volumeCurrentSfx = sfxSlider.value;
+        AudioManager.volumeCurrentBgm = bgmSlider.value;
+
+    }
+}
